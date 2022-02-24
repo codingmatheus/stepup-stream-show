fed2a38d2cf7a4ba382fe49cf55077822c8ef164
Matheus <guimathe@amazon.co.uk>
2022-02-24 17:37:33 +0000

    sam template and code for sfn and lambda

---
+++ episode-1/code/functions/moviewatchfinderservice/app.py
@@ -0,0 +1,18 @@
+movies = []
+
+def populate():
+    movies.append({"Title": "Titanic", "Platform": "Netflix"})
+    movies.append({"Title": "Titanic", "Platform": "Amazon Prime"})
+    movies.append({"Title": "Saw", "Platform": "Netflix"})
+
+populate()
+
+def lambda_handler(event, context):
+
+    result = []
+
+    for movie in movies:
+        if movie["Title"] == event["Title"]:
+            result.append(movie)
+            
+    return result
